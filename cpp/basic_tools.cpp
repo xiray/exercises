36dde76480c863104a9f91eb960f99783f7b6680
ray <ray@xray.org>
2026-07-11 16:56:50 +0800

    improve caculate, and make it can do minus

---
+++ cpp/basic_tools.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// is set, always return bool
+bool is_sign(char a) {
+  if(a == '+' || a == '-' || a == '*' || a == '/')
+    return true;
+  else
+    return false;
+}
+
+bool is_number(char a) {
+  if(a >= '0' && a <= '9')
+    return true;
+  else
+    return false;
+}
+
+bool is_prime(long long a) {
+  if(a == 0) return false;
+  if(a == 1) return false;
+  if(a == 2) return true;
+  for(long long i = 2; i <= sqrt(a); i++) {
+    if(a % i == 0)
+      return false;
+  }
+  return true;
+}
+
+
+// to and turn set
+int str_to_int(string a) {
+  int x = 0;
+  int len = a.size();
+  for(int i = 0; i < len; i++) {
+    x *= 10;
+    x += a[i] - 48;
+  }
+  return x;
+}
--- cpp/myself/expression_evaluation.cpp
+++ cpp/myself/expression_evaluation.cpp
@@ -1,54 +1,83 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+bool is_sign(char a) { return (a == '+' || a == '-' || a == '*' || a == '/'); }
+
+bool is_number(char a) { return (a >= '0' && a <= '9'); }
 
 int str_to_int(string a) {
   int x = 0;
-  int len = a.size();
-  for(int i = 0; i < len; i++) {
-    x *= 10;
-    x += a[i] - 48;
+  for (char c : a) {
+    x = x * 10 + (c - '0');
   }
   return x;
 }
 
 int main() {
   string a, num_1, num_2, sign;
-  bool space = false;
+  bool space = false, have_sign = false;
+  bool m1 = false, m2 = false;
   getline(cin, a);
   int len = a.size(), sign_place, n1, n2;
 
-  for(int i = 0; i < len; i++) {
-    if(a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/') {
+  for (int i = 0; i < len; i++) {
+    if (i != 0 && is_sign(a[i]) && have_sign == false) {
       sign = a[i];
+      have_sign = true;
       sign_place = i;
+      continue;
     }
-    if(a[i] == ' ') {
+    if (i == 0 && a[i] == '-')
+      m1 = true;
+    if (i == 1 && a[i] == '-')
+      m1 = true;
+    if (have_sign && a[i] == '-')
+      m2 = true;
+    if (a[i] == ' ')
       space = true;
-    }
-    if(a[i] < 48 || a[i] > 57) {
+    if (!(is_number(a[i]) || is_sign(a[i]) || a[i] == ' ')) {
       cout << "wrong input";
       return 0;
     }
-  } 
+  }
 
-  if(!space) {
-    num_1 = a.substr(0, sign_place );
-    num_2 = a.substr(sign_place + 1, len - sign_place);
+  int l1 = 0, len1 = sign_place;
+  int l2 = sign_place + 1, len2 = len - sign_place - 1;
+
+  if (space) {
+    len1--;
+    len2--;
+    l2++;
   }
-  if(space) {
-    num_1 = a.substr(0, sign_place - 1);
-    num_2 = a.substr(sign_place + 2, len - sign_place);
+  if (m1) {
+    l1++;
+    len1--;
   }
+  if (m2) {
+    len2--;
+    l2++;
+  }
+
+  num_1 = a.substr(l1, len1);
+  num_2 = a.substr(l2, len2);
+
   n1 = str_to_int(num_1);
   n2 = str_to_int(num_2);
+  if (m1)
+    n1 *= -1;
+  if (m2)
+    n2 *= -1;
 
   float ans;
-  if(sign == "+") ans = n1 + n2;
-  if(sign == "-") ans = n1 - n2;
-  if(sign == "*") ans = n1 * n2;
-  if(sign == "/") {
-    if(n2 != 0)
-      ans = n1 / 1.0 / n2;
+  if (sign == "+")
+    ans = n1 + n2;
+  else if (sign == "-")
+    ans = n1 - n2;
+  else if (sign == "*")
+    ans = n1 * n2;
+  else if (sign == "/") {
+    if (n2 != 0)
+      ans = (float)n1 / n2;
     else {
       cout << "the second number can't be 0";
       return 0;
@@ -56,4 +85,4 @@ int main() {
   }
   cout << ans;
   return 0;
-} 
+}
